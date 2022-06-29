59f379e037ce42faec0d14502d57b6ece04b41b6
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2022-06-29 10:34:36 -0300

    feat: explain interface segregation principle

---
+++ isp.ts
@@ -0,0 +1,24 @@
+//interface segregation principle
+interface Database {
+    storeData(data: any);
+  }
+  
+  interface RemoteDatabase {
+    connect(uri: string);
+  }
+  
+  class SQLDatabase implements Database, RemoteDatabase {
+    connect(uri: string) {
+      // connecting...
+    }
+  
+    storeData(data: any) {
+      // Storing data...
+    }
+  }
+  
+  class InMemoryDatabase implements Database {
+    storeData(data: any) {
+      // Storing data...
+    }
+  }
