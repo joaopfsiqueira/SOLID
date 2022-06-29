1f46913ea68f0d6330881a78f4d15f0774270c8b
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2022-06-29 10:33:33 -0300

    feat: explain Princípio de substituição de Liskov

---
+++ lsp.ts
@@ -0,0 +1,23 @@
+//Princípio de substituição de Liskov
+
+class Bird {}
+
+class FlyingBird extends Bird {
+  fly() {
+    console.log('Fyling...');
+  }
+}
+
+class Eagle extends FlyingBird {
+  dive() {
+    console.log('Diving...');
+  }
+}
+
+const eagle = new Eagle();
+eagle.fly();
+eagle.dive();
+
+class Penguin extends Bird {
+  // Problem: Can't fly!
+}
